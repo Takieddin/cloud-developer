642585857054804836502f3057ee8494d7bcfeec
Takieddin <t.fellag@esi-sba.dz>
2021-04-19 15:44:06 +0100

    specifying req and res types

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import {Request,Response} from 'express';
 import bodyParser from 'body-parser';
 import { filterImageFromURL, deleteLocalFiles } from './util/util';
 
@@ -28,7 +29,7 @@ import { filterImageFromURL, deleteLocalFiles } from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
-  app.get('/filteredimage', async (req, res) => {
+  app.get('/filteredimage', async (req:Request, res:Response) => {
 
     let image_url = req.query.image_url;
     if (!image_url) {
