abb7b016bc1e8a0fbad879ae6b70658be3ddd0e0
Takieddin <t.fellag@esi-sba.dz>
2021-06-07 03:53:08 +0100

    update ts

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/updateTodo.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/updateTodo.ts
@@ -4,10 +4,42 @@ import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } f
 
 import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
 
+import { TodoItem } from '../../models/TodoItem'
+
+import { getUserId } from "../utils";
+import * as AWS from 'aws-sdk'
+
+
+
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
   const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
+    // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
 
-  // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-  return undefined
+  const userId = getUserId(event)
+  var docClient = new AWS.DynamoDB.DocumentClient()
+  const toDoTable = 'toDoTable'
+
+
+
+
+
+
+  var params = {
+    TableName: toDoTable,
+    Key: {
+      "todoId": todoId,
+      "userId": userId
+    },
+    UpdateExpression: "set name = :name, dueDate=:dueDate, done=:done",
+    ExpressionAttributeValues: {
+      name: updatedTodo.name,
+      dueDate: updatedTodo.dueDate,
+      done: updatedTodo.done,
+    },
+    ReturnValues: "UPDATED_NEW"
+  }
+  return docClient.update(params) 
 }
+
+
