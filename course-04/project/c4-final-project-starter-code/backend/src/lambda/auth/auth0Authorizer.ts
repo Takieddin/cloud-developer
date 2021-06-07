b836b05e77945cc05852f09cdf34c8c8db68c88a
Takieddin <t.fellag@esi-sba.dz>
2021-06-07 01:02:30 +0100

    commit

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/auth/auth0Authorizer.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/auth/auth0Authorizer.ts
@@ -7,12 +7,21 @@ import Axios from 'axios'
 import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
 
+import jwksClient from 'jwks-rsa';
+
+
 const logger = createLogger('auth')
 
 // TODO: Provide a URL that can be used to download a certificate that can be used
 // to verify JWT token signature.
 // To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = '...'
+const jwksUrl = 'https://ta9i.auth0.com/.well-known/jwks.json'
+
+const jwkClient = jwksClient({
+  strictSsl: true,
+  jwksUri: jwksUrl //or wherever you have your configs
+});
+
 
 export const handler = async (
   event: CustomAuthorizerEvent
@@ -61,7 +70,24 @@ async function verifyToken(authHeader: string): Promise<JwtPayload> {
   // TODO: Implement token verification
   // You should implement it similarly to how it was implemented for the exercise for the lesson 5
   // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  return undefined
+
+
+  
+  const header = jwt.header;
+  const kid = header.kid;
+  let cert;
+
+  jwkClient.getSigningKey(kid, (err, key) => {    
+    cert = key.publicKey;    
+  });
+
+ return  verify(
+    token,           // Token from an HTTP header to validate
+    cert,            // A certificate copied from Auth0 website
+    { algorithms: ['RS256'] } // We need to specify that we use the RS256 algorithm
+) as JwtPayload
+
+  
 }
 
 function getToken(authHeader: string): string {
